6cb73758962f93845cb9744bee0f0ae37c483f99
QiuCheng <tegry@126.com>
2024-01-26 09:55:58 +0800

    Update xiaomi.py

--- function/xiaomi_sports/xiaomi.py
+++ function/xiaomi_sports/xiaomi.py
@@ -340,7 +340,7 @@ def main(_user, _passwd, _step):
 
     response = requests.post(url, data=data, headers=head).json()
     # print(response)
-    result = f"{user[:4]}****{user[-4:]}: [{now}] 修改步数（{step}）" + response['message']
+    result = f"{user[:4]}****{user[-4:]}: [{now}] 修改步数（{_step}）" + response['message']
     print(result)
     return result
 
